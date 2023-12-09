496e05585dc95e7cccbe28cbcb53381b66b36a76
Nataniel Junior <fnatanieljunior@gmail.com>
2023-12-09 02:52:39 -0300

    Login com Google feito, ainda falta algumas alteraçoes no clientId

--- front-end/src/components/login/login.jsx
+++ front-end/src/components/login/login.jsx
@@ -6,18 +6,29 @@ import github from '../../assets/001-github.png'
 import '../login/login.scss'
 import { BrowserRouter as Router,Link } from 'react-router-dom'
 import { GoogleLogin } from '@react-oauth/google'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 export function Login(){
     const [Verify, setVerify] = useState(true)
     const handleRegister = ()=>{
        setVerify(!Verify) 
     };
+    
+
+    const handleSucessLogin = (response)=>{
+        console.log(response)
+    }
+
+    const handleErrorLogin = (error)=>{
+        console.log(error)
+    }
 
     return(
         <Router>
         <main>
             <section className={Verify? 'login':'register-user'} >
                 <h2>{Verify? 'Login':'Register'}</h2>
-                <form action="">
+                {Verify? (
+                    <form action="">
                     <div className="email input">
                         <img src={email} alt="" />
                         <input type="email" id="email" name="email" placeholder="Email" required/>
@@ -29,12 +40,35 @@ export function Login(){
                     </div>
                     <button type="submit" className="btn btn-primary input">Login</button>
                 </form>
+                ):(
+                    <form action="">
+                    <div className="email input">
+                        <img src={email} alt="" />
+                        <input type="email" id="email" name="email" placeholder="Email" required/>
+                    </div>
+
+                    <div className="password input">
+                        <img src={lock} alt=""/>
+                        <input type="password" name="password" id="password" placeholder="Password" required/>
+                    </div>
+                    <button type="submit" className="btn btn-primary input">Criar conta</button>
+                </form>
+                )}
+                
 
                 <p>or continue with these social profile</p>
 
 
                 <section className='login-tecnologies'>
-                    <span className='google'><img src={google} alt="" /></span>
+                    <span className='google'>
+                    <GoogleOAuthProvider clientId='536913788151-kp714umuvpu1iu00gavg63i1gn263aa9.apps.googleusercontent.com'>
+                        <GoogleLogin
+                        buttonText='Login with Goolgle'
+                        onScriptLoadSuccess={handleSucessLogin}
+                        onScriptLoadError={handleErrorLogin}
+                        />
+                    </GoogleOAuthProvider><img src={google} alt="" />
+                    </span>
                     <span className='github'><img src={github} alt="" /></span>
                 </section>
 
