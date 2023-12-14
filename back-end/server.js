cd261193a17d6b5eafe7db4d1b85b2433a7aa818
Nataniel Junior <fnatanieljunior@gmail.com>
2023-12-14 19:02:04 -0300

    foi adicionado ao servidor a rota de registro de  usuário

--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 const mongosee = require('mongoose');
+const { default: mongoose } = require('mongoose');
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
@@ -11,30 +12,53 @@ const CreateData = new mongosee.Schema({
     email: String,
     password: String
 });
-
+const EditionData = new mongosee.Schema({
+    nameEdit:String, 
+    BioEdit: String,
+    phoneEdit:String,
+    emailEdit: String,
+    passwordEdit:String,
+})
 let Data = mongosee.model("data", CreateData);
-
+let Edition = mongoose.model("edit", EditionData) 
 app.get('/',  (req,res)=>{
     return res.send('hello world')
 });
-
-app.post('/Login', async (req,res)=>{
+app.post('/', async (req,res)=>{
+    try{
+        const {emailRegister,passwordRegister} = req.body;
+        const Register = new Data({
+            email:emailRegister,
+            password:passwordRegister,
+        })
+        const saveUser = await Register.save()
+        return res.json({message:"Usuário salvo com sucesso!!"})
+    }
+    catch(erro){
+        console.error('Cadastro não realizado, preencha os dados corretamente!')
+    }
+})
+app.post('/Feed', async (req,res)=>{
     try{
         const {emailLogin,passwordLogin} = req.body
         const NewData = new Data({
         email:emailLogin,
-        passwor:passwordLogin
+        password:passwordLogin
     });
-        Data.find({email:emailLogin, password:passwordLogin},(error,data)=>{
-            if(error) return console.error('Usuário não encontrado')
-            console.log(data)
+        const search = await Data.find({email:emailLogin, password:passwordLogin},(error,data)=>{
+            if(error){
+                return res.send('Usuário não encontrado, se cadastre por favor!!')
+            }else{
+                console.log(data)
+                return res.send('Login efetuado com sucesso!!')
+            } 
         })
+        console.log(search)
     }
     catch(erro){
-
+        return res.json({message: "Erro ao fazer login!!"})
     }
 })
-
 app.listen(port,()=>{
     console.log(`servidor aberto na porta ${port}`);
 });
