4e704e0f2e4ceefce100f3c4663e4e3d793ee929
Nataniel Junior <fnatanieljunior@gmail.com>
2023-12-27 17:39:14 -0300

    port trocada de 3000, para 3001.

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const mongosee = require('mongoose');
 const { default: mongoose } = require('mongoose');
 require('dotenv').config()
